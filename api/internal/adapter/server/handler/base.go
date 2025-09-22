1c978426999a65ad0310a7c41e0a2ea25478953b
Leinadium <daniel.sch.guima@gmail.com>
2025-09-22 09:28:11 -0300

    feat: adding search

--- api/internal/adapter/server/handler/base.go
+++ api/internal/adapter/server/handler/base.go
@@ -3,16 +3,19 @@ package handler
 import "leinadium.dev/wca-ranking/internal/core/port"
 
 type ServerHandler struct {
-	stateService port.StateService
-	syncService  port.SyncService
+	stateService  port.StateService
+	syncService   port.SyncService
+	searchService port.SearchService
 }
 
 func NewServerHandler(
 	stateService port.StateService,
 	syncService port.SyncService,
+	searchService port.SearchService,
 ) *ServerHandler {
 	return &ServerHandler{
-		stateService: stateService,
-		syncService:  syncService,
+		stateService:  stateService,
+		syncService:   syncService,
+		searchService: searchService,
 	}
 }
---
+++ api/internal/adapter/server/handler/search.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"context"
+
+	"leinadium.dev/wca-ranking/internal/adapter/server/schema"
+	"leinadium.dev/wca-ranking/internal/core/domain"
+	"leinadium.dev/wca-ranking/pkg/utils"
+)
+
+func (s *ServerHandler) GetSearch(
+	ctx context.Context,
+	request schema.GetSearchRequestObject,
+) (schema.GetSearchResponseObject, error) {
+	search, err := s.searchService.Search(ctx, request.Params.S)
+	if err != nil {
+		return schema.GetSearch500JSONResponse{
+			Code:    &ErrDefaultCode,
+			Message: &ErrDefaultMessage,
+		}, nil
+	} else {
+		return schema.GetSearch200JSONResponse(
+			utils.MapNotNull(search, func(s *domain.SearchResult) schema.SearchItem {
+				return schema.SearchItem{
+					WcaId:   (*string)(&s.WCAID),
+					WcaName: &s.Name,
+					StateId: &s.StateID.String,
+				}
+			}),
+		), nil
+	}
+}
